b51f2635e5917e9f95dd56f62ded90ce546a3968
jin <jin@DESKTOP-U0D88KQ>
2020-09-18 15:23:04 +0900

    진이zx

--- TEST64/src/TEST.java
+++ TEST64/src/TEST.java
@@ -4,7 +4,9 @@ public class TEST {
 private void sysou() {
 	// TODO Auto-generated method stub
 	///수녕이 스타 몬한다
-	
+	     //    - >  ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ
+//  	ㅕㅓ허ㅓ허허하ㅓ라허ㅡㅓㅅ요선느ㅛ곤ㄱ스노ㅗㄴ소뇩
+	///이ㅏ내다ㅣ나ㅣ나ㅣ나ㅣㄴ
 	//아대니아댜무
 }	
 
